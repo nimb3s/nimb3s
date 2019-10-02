96b3232a94aead2d3fe205ae0a2d5fbb71b8247e
ktabarez <kaintabarez@gmail.com>
2019-10-02 03:03:06 -0700

    build(spa-nimb3s) added platform to debug output

--- build/vars.ps1
+++ build/vars.ps1
@@ -2,11 +2,13 @@
 $isRunningOnBuildServer = IIf $env:APPVEYOR $true $false
 $buildFolder = IIf $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_BUILD_FOLDER (Get-Location)
 $prCommitId = IIf $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT (git rev-parse --verify HEAD)
+$platform = IIf $env:PLATFORM  $env:PLATFORM  "localhost"
 
 #projects
 $spaDir = Join-Path -Path $buildFolder -ChildPath "src/spas" 
 
 Write-Output "Running on build server: $($isRunningOnBuildServer)"
+Write-Output "Platform: $($platform)"
 Write-Output "Build folder: $($buildFolder)"  
 Write-Output "PR Commit ID: $($prCommitId)"
 Write-Output "SPA Dir: $($spaDir)"
