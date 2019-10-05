770af4628ba79917907ee90bac505d1cc65186a1
ktabarez <kaintabarez@gmail.com>
2019-10-05 01:20:30 -0700

    Update environment.ts

--- src/spas/apps/nimb3s/src/environments/environment.ts
+++ src/spas/apps/nimb3s/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDPiHc5cnYLfaA6QsEaDkiY00ISZy7xJ6k",
-    authDomain: "jw-admin.firebaseapp.com",
-    databaseURL: "https://jw-admin.firebaseio.com",
-    projectId: "jw-admin",
-    storageBucket: "jw-admin.appspot.com",
-    messagingSenderId: "222203102261",
-    appId: "1:222203102261:web:015ceca0565070eb471d7c",
-    measurementId: "G-C5QVKB3Z37"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
