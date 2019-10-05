5920bf2b9b0f907c33f5ce734660b3336b763a23
ktabarez <kaintabarez@gmail.com>
2019-10-04 22:58:47 -0700

    comp(nimb3s): firebase hosting config

--- src/spas/apps/nimb3s/src/app/app.module.ts
+++ src/spas/apps/nimb3s/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
