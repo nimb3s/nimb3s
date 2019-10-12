e06513d58980099b6f1739901cf35e12e4009f31
ktabarez <kaintabarez@gmail.com>
2019-10-12 15:24:24 -0700

    comp(ci/cd) gitworkflow build numbers and deploy targets defined

--- build/funcs.ps1
+++ build/funcs.ps1
@@ -1,23 +1,4 @@
 Function IIf($If, $IfTrue, $IfFalse) {
-    If ($If) {If ($IfTrue -is "ScriptBlock") {&$IfTrue} Else {$IfTrue}}
-    Else {If ($IfFalse -is "ScriptBlock") {&$IfFalse} Else {$IfFalse}}
-}
-
-Function Get-GitTagName($tagName) {
-    if ($null -eq $tagName) {
-        throw "Git tag missing! Tag your commit when merging develop to master: $tagName"
-    }
-    Write-Output "11"
-    if ($tagName -match "^v") {
-    
-        try {
-            $tagName -replace "^v", '' | ConvertFrom-String -Delimiter '\.' -PropertyNames Major, Minor, Patch
-            return $tagName
-        }
-        catch {
-            throw "Failed to parse Git tag name: $($tagName)"    
-        }       
-    } else {
-        throw "Git tag name must start with 'v.' Example: v1.0.0: $($tagName)"
-    }
+    if ($If) {if ($IfTrue -is 'ScriptBlock') {&$IfTrue} else {$IfTrue}}
+    else {if ($IfFalse -is 'ScriptBlock') {&$IfFalse} else {$IfFalse}}
 }
--- build/vars.ps1
+++ build/vars.ps1
@@ -1,63 +1,85 @@
-$developBranchPrefix = "-rc"
-$masterBranchPrefix = ""
-#env vars
-$platform = IIf $env:PLATFORM  $env:PLATFORM  "localhost"
+$undefined = 'UNDEFINED'
+
+#deploy
+$prodDeployTarget = 'PRODUCTION'
+$developDeployTarget = 'DEVELOP'
+$stagingDeployTarget = 'STAGING'
+$localhostDeployTarget = 'LOCALHOST'
+
+
+#context
+$platform = IIf $env:PLATFORM  $env:PLATFORM  $localhostDeployTarget
 $isRunningOnBuildServer = IIf $env:APPVEYOR $true $false
 $buildFolder = IIf $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_BUILD_FOLDER (Get-Location)
+$currentBranch = (git rev-parse --abbrev-ref HEAD)
 
+#pull request
+$isPullRequest = $false
 $prCommitId = IIf $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT (git rev-parse --verify HEAD)
+$prBranchSource = IIf $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH $undefined
+$prBranchDestination = IIf $env:APPVEYOR_REPO_BRANCH $env:APPVEYOR_REPO_BRANCH $undefined
 
-$prBranchSource = IIf $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH 'UNKNOWN'
-$prBranchDestination = IIf $env:APPVEYOR_REPO_BRANCH $env:APPVEYOR_REPO_BRANCH 'UNKNOWN'
-$buildVersion = IIf $env:APPVEYOR_BUILD_VERSION $env:APPVEYOR_BUILD_VERSION  $prCommitId
-$buildTagName = IIf $env:APPVEYOR_REPO_TAG_NAME  $env:APPVEYOR_REPO_TAG_NAME  'UNKNOWN'
+if ($prBranchSource -ne $undefined) {
+    $isPullRequest = $true
+}
 
-#determine master build version
-if ($prBranchSource -eq 'develop' -and $prBranchDestination -eq "master") {
-    $currentGitTag = Get-GitTagName $buildTagName
-} elseif ($prBranchSource -match "^feature/") {
-    $ErrorActionPreference = "SilentlyContinue";
-    $previousGitTag = Get-GitTagName (git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))   
-    $ErrorActionPreference = "Stop";
+#build meta data
+$buildNumber = IIf $env:APPVEYOR_BUILD_NUMBER $env:APPVEYOR_BUILD_NUMBER  '0.0.0'
+$buildStartedByTag = IIf $env:APPVEYOR_REPO_TAG $env:APPVEYOR_REPO_TAG $undefined
+$buildTagName = IIf $env:APPVEYOR_REPO_TAG_NAME  $env:APPVEYOR_REPO_TAG_NAME  $undefined
+$gitVersion = gitversion | ConvertFrom-Json
 
-    if ($null -ne $previousGitTag) {
-        if([int]$currentGitTag.Major -gt [int]$previousGitTag.Major) {
-            Write-Output "wetf"
-        }
+#release
+$releaseDir = Join-Path -Path $buildFolder -ChildPath 'release'
+$artifactsDir = Join-Path -Path $buildFolder -ChildPath 'artifacts'
+
+#projects
+$spaDir = Join-Path -Path $buildFolder -ChildPath 'src/spas' 
+$distDir = Join-Path -Path $spaDir -ChildPath 'dist'
+
+#environment
+$envDeployTarget = $localhostDeployTarget
+
+if ($isRunningOnBuildServer -eq $true) {
+    if ($prBranchDestination -eq 'master' -or $currentBranch -eq 'master') {
+        $envDeployTarget = $prodDeployTarget
+    } elseif ($prBranchDestination -eq 'develop' -or $currentBranch -match '^feature/') {
+        $envDeployTarget = $developDeployTarget
+    } elseif ($prBranchDestination -match '^release/'-or $currentBranch -match '^hotfix/') {
+        $envDeployTarget = $stagingDeployTarget
     }
 }
 
-if ($prBranchDestination -match 'develop') {
-    $buildVersion = "$($prCommitId)-rc"
-} elseif ($prBranchDestination -match 'master') {
-    if ($null -eq $buildTagName) {
-        throw ""
-        $host.SetShouldExit(1)
-    }
-} 
-
-$buildNumber = IIf $env:APPVEYOR_BUILD_NUMBER $env:APPVEYOR_BUILD_NUMBER  "0.0.0"
-
-#release
-$releaseDir = Join-Path -Path $buildFolder -ChildPath "release"
-$artifactsDir = Join-Path -Path $buildFolder -ChildPath "artifacts"
-
-#projects
-$spaDir = Join-Path -Path $buildFolder -ChildPath "src/spas" 
-$distDir = Join-Path -Path $spaDir -ChildPath "dist"
-
-Write-Output "Running on build server: $($isRunningOnBuildServer)"
+Write-Output ""
 Write-Output "Platform: $($platform)"
+Write-Output "Running on build server: $($isRunningOnBuildServer)"
+Write-Output "Current branch: $($currentBranch)"
+Write-Output "Deploy target: $($envDeployTarget)"
+Write-Output ""
+
+Write-Output "DIRECTORIES:"
+Write-Output ""
 Write-Output "Build folder: $($buildFolder)"
 Write-Output "Release dir: $($releaseDir)"
 Write-Output "Artifacts dir: $($artifactsDir)"
 Write-Output "Dist dir: $($distDir)"
 Write-Output "SPA Dir: $($spaDir)"
+Write-Output ""
+
+Write-Output "PULL REQUEST:"
+Write-Output ""
+Write-Output "PR is pull request: $($isPullRequest)"
 Write-Output "PR Commit ID: $($prCommitId)"
 Write-Output "PR branch source: $($prBranchSource)"
 Write-Output "PR branch destination: $($prBranchDestination)"
+Write-Output ""
+
+Write-Output "BUILD:"
+Write-Output ""
 Write-Output "Build number: $($buildNumber)"
-Write-Output "Build version: $($buildVersion)"
 Write-Output "Build tag name: $($buildTagName)"
-Write-Output "Develop prefix: $($developBranchPrefix)"
-Write-Output "Master prefix: $($masterBranchPrefix)"
+Write-Output "Build started by pushed tag name: $($buildStartedByTag)"
+Write-Output "Git Version:"
+$gitVersion | Select-Object *
+Write-Output ""
+
