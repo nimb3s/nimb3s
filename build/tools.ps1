5e014b1824cb190cda2f4a91f16dbdd24b22304f
ktabarez <kaintabarez@gmail.com>
2019-10-13 23:35:51 -0700

    ci(tools) removed 7zip from install

--- build/tools.ps1
+++ build/tools.ps1
@@ -4,12 +4,6 @@ Write-Output "******************************************"
 
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-$7zipVersion = Get-Package -ProviderName Programs -IncludeWindowsInstaller | Where-Object Name -like '*7-Zip*' | Where-Object  Version -like "*19.0*"
-
-If($7zipVersion.Version -notlike '*19.00.00.0*') {
-    choco install 7zip.install   
-}
-
 Write-Output "******************************************"
 Write-Output "INSTALLING TOOLS: FINISHED"
 Write-Output "******************************************"
