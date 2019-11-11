222fc2825db4db0bbf0b57aac2354d0ef0d37b2a
ktabarez <kaintabarez@gmail.com>
2019-11-11 11:21:54 -0800

    spas(nimb3s) added firebase config

--- src/spas/apps/nimb3s/src/environments/environment.prod.ts
+++ src/spas/apps/nimb3s/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyDq_fvoTRA5rV2DtCHpE8YFIohxMM4psr0",
+    authDomain: "prod-nimb3s.firebaseapp.com",
+    databaseURL: "https://prod-nimb3s.firebaseio.com",
+    projectId: "prod-nimb3s",
+    storageBucket: "prod-nimb3s.appspot.com",
+    messagingSenderId: "457377159548",
+    appId: "1:457377159548:web:1871f98427f2dee2826cf9",
+    measurementId: "G-77KXPVPCLP"
   }
 };
--- src/spas/apps/nimb3s/src/environments/environment.ts
+++ src/spas/apps/nimb3s/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyBqB2R01lg7CEyQqhMs59cIYb6TRoscC6U",
+    authDomain: "dev-nimb3s.firebaseapp.com",
+    databaseURL: "https://dev-nimb3s.firebaseio.com",
+    projectId: "dev-nimb3s",
+    storageBucket: "dev-nimb3s.appspot.com",
+    messagingSenderId: "95568800587",
+    appId: "1:95568800587:web:4b5d43daaf2e415421d6ce",
+    measurementId: "G-WHJMY9Q59G"
   }
 };
 
