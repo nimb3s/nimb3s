9debcd1d598b17a3c53cbd4da521b64d15110ec7
ktabarez <kaintabarez@gmail.com>
2019-12-31 22:30:17 -0800

    spas(lib:layout) new toolbar component

--- src/spas/libs/shared/ui/layout/src/lib/toolbar/toolbar.module.ts
+++ src/spas/libs/shared/ui/layout/src/lib/toolbar/toolbar.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ToolbarComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ToolbarComponent]
 })
 export class ToolbarModule { }
