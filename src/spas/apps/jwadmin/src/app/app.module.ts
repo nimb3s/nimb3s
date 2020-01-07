fd887c11183b1474119e17b7970a4fa67d63abcb
ktabarez <kaintabarez@gmail.com>
2020-01-06 16:52:16 -0800

    test toolbar component in app

--- src/spas/apps/jwadmin/src/app/app.module.ts
+++ src/spas/apps/jwadmin/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
+import { SharedUiLayoutModule } from '@nimb3s/shared/ui/layout';
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent,],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase, 'dev-jwadmin')
+    AngularFireModule.initializeApp(environment.firebase, 'dev-jwadmin'),
+    SharedUiLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/spas/libs/shared/ui/layout/src/lib/shared-ui-layout.module.ts
+++ src/spas/libs/shared/ui/layout/src/lib/shared-ui-layout.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarModule } from './toolbar/toolbar.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ToolbarModule
+  ],
+  exports: [
+    ToolbarComponent
   ]
 })
 export class SharedUiLayoutModule {}
