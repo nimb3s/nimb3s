0851efd494b1c3560c6f4b8120d0ded737191880
ktabarez <kaintabarez@gmail.com>
2020-02-29 23:13:15 -0800

    (spas:feature:member): fixed prefix config

--- src/spas/libs/member-management/feature/src/lib/member-management/member-management.component.ts
+++ src/spas/libs/member-management/feature/src/lib/member-management/member-management.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserProfile } from '@nimb3s/member-management/domain';
 
 @Component({
-  selector: 'member-management',
+  selector: 'feature-member-management',
   templateUrl: './member-management.component.html',
   styleUrls: ['./member-management.component.scss']
 })
