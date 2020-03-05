dec31e3eeca72b3dbaa3ff82c2c2b55131114591
ktabarez <kaintabarez@gmail.com>
2020-03-04 19:18:03 -0800

    (spas:lib:ui): card component

--- src/spas/libs/shared/ui/layout/src/lib/card/card.component.ts
+++ src/spas/libs/shared/ui/layout/src/lib/card/card.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
 
+// noinspection TsLint
 @Component({
   selector: 'ui-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
+  host: { 'class': 'ui-card' },
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FuryCard {
 }
