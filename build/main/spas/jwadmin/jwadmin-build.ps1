99a95b22062e558e1b1be5a2e84e2c8ac81c2fe6
ktabarez <kaintabarez@gmail.com>
2020-11-07 02:15:14 -0800

    do not deploy jwadmin, udpated default project and dist path

--- build/main/spas/jwadmin/jwadmin-build.ps1
+++ build/main/spas/jwadmin/jwadmin-build.ps1
@@ -1,49 +1,49 @@
-$appName = "JwAdmin"
-$friendlyAppName = "SPA $($appName)"
-$nugetPkgId = "$($appName).Spa"
-$appSpaDir = Join-Path -Path $spaDir -ChildPath "apps/$appName"
-
-if ($envDeployTarget -eq $stagingDeployTarget) {
-    $npmBuildScript = "build:$($appName.ToLower())-stage"
-} elseif ($envDeployTarget -eq $prodDeployTarget) {
-    $npmBuildScript = "build:$($appName.ToLower())-prod"
-} else {
-    $npmBuildScript = "build:$($appName.ToLower())-dev"
-}
-
-Build-AngularApp `
-    -AppName $friendlyAppName `
-    -SpaDirectory $spaDir `
-    -SiteRootDirectory "apps/$($appName.ToLower())/*" `
-    -DistDirectory $distDir `
-    -NpmInstallScript "build:install" `
-    -NpmSiteBuildScript $npmBuildScript
-
-Publish-ReleasePackage `
-    -AppName $friendlyAppName `
-    -BuildDirectory $buildDir `
-    -IsExecutingOnBuildServer $isRunningOnBuildServer `
-    -ArtifactsDirectory $artifactsDir `
-    -ReleaseDirectory $releaseDir `
-    -GitVersioning $gitVersion `
-    -NugetPackageId $nugetPkgId `
-    -NugetPackageDescription "$($appName) Single Page App" `
-    -NugetTags "nimb3s jwadmin angular web spa" `
-    -NugetFileTargets @( ,("<file src=`"..\src\spas\dist\apps\$($appName.ToLower())\**\*`" target=`"dist`" />")) `
-    -NugetFeedApiKey $nugetApiKey `
-    -NugetFeedUrl $nugetUrl
-    
-Publish-FirebaseSite `
-    -AppName $appName `
-    -SpaDirectory $spaDir `
-    -AppSpaDirectory $appSpaDir `
-    -BuildDirectory $buildDir `
-    -IsExecutingOnBuildServer $isRunningOnBuildServer `
-    -DeployTarget $envDeployTarget `
-    -LocalDeployTarget $localhostDeployTarget `
-    -TargetDefault "default-$($appName.ToLower())" `
-    -TargetStage "stage-$($appName.ToLower())" `
-    -TargetProd "prod-$($appName.ToLower())" `
-    -FirebaseKey $firebaseToken `
-    -NugetPackageId $nugetPkgId `
-    -GitVersioning $gitVersion
+#$appName = "JwAdmin"
+#$friendlyAppName = "SPA $($appName)"
+#$nugetPkgId = "$($appName).Spa"
+#$appSpaDir = Join-Path -Path $spaDir -ChildPath "apps/$appName"
+#
+#if ($envDeployTarget -eq $stagingDeployTarget) {
+#    $npmBuildScript = "build:$($appName.ToLower())-stage"
+#} elseif ($envDeployTarget -eq $prodDeployTarget) {
+#    $npmBuildScript = "build:$($appName.ToLower())-prod"
+#} else {
+#    $npmBuildScript = "build:$($appName.ToLower())-dev"
+#}
+#
+#Build-AngularApp `
+#    -AppName $friendlyAppName `
+#    -SpaDirectory $spaDir `
+#    -SiteRootDirectory "apps/$($appName.ToLower())/*" `
+#    -DistDirectory $distDir `
+#    -NpmInstallScript "build:install" `
+#    -NpmSiteBuildScript $npmBuildScript
+#
+#Publish-ReleasePackage `
+#    -AppName $friendlyAppName `
+#    -BuildDirectory $buildDir `
+#    -IsExecutingOnBuildServer $isRunningOnBuildServer `
+#    -ArtifactsDirectory $artifactsDir `
+#    -ReleaseDirectory $releaseDir `
+#    -GitVersioning $gitVersion `
+#    -NugetPackageId $nugetPkgId `
+#    -NugetPackageDescription "$($appName) Single Page App" `
+#    -NugetTags "nimb3s jwadmin angular web spa" `
+#    -NugetFileTargets @( ,("<file src=`"..\src\spas\dist\apps\$($appName.ToLower())\**\*`" target=`"dist`" />")) `
+#    -NugetFeedApiKey $nugetApiKey `
+#    -NugetFeedUrl $nugetUrl
+#    
+#Publish-FirebaseSite `
+#    -AppName $appName `
+#    -SpaDirectory $spaDir `
+#    -AppSpaDirectory $appSpaDir `
+#    -BuildDirectory $buildDir `
+#    -IsExecutingOnBuildServer $isRunningOnBuildServer `
+#    -DeployTarget $envDeployTarget `
+#    -LocalDeployTarget $localhostDeployTarget `
+#    -TargetDefault "default-$($appName.ToLower())" `
+#    -TargetStage "stage-$($appName.ToLower())" `
+#    -TargetProd "prod-$($appName.ToLower())" `
+#    -FirebaseKey $firebaseToken `
+#    -NugetPackageId $nugetPkgId `
+#    -GitVersioning $gitVersion
