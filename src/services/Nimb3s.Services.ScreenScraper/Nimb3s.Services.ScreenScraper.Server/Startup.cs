d59f6bbc4e122ba3efcaec0501072a1c3d1f3f2a
Alex Aveldanez <alexsaveldanez@gmail.com>
2020-12-22 18:24:58 -0800

    added swagger to API projects

--- src/services/Nimb3s.Services.ScreenScraper/Nimb3s.Services.ScreenScraper.Server/Startup.cs
+++ src/services/Nimb3s.Services.ScreenScraper/Nimb3s.Services.ScreenScraper.Server/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Nimb3s.Services.ScreenScraper.Server
 {
@@ -26,6 +27,11 @@ namespace Nimb3s.Services.ScreenScraper.Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Scraper Server API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,9 @@ namespace Nimb3s.Services.ScreenScraper.Server
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Scraper Server API v1"); });
 
             app.UseEndpoints(endpoints =>
             {
